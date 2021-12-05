2c2e60fea52a5fbc5cf403cf29df28002f743dcc
darektoa <darektoa@gmail.com>
2021-12-05 21:58:02 +0700

    Added: Register API route

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,9 +3,28 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use Exception;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    public function register(Request $request) {
+        try{
+            
+
+            return response()->json([
+                'status'    => 200,
+                'message'   => 'OK',
+                'data'      => []
+            ]);
+        }catch(Exception $err) {
+            $errCode    = $err->getCode() ?: 400;
+            $errMessage = $err->getMessage();
+            
+            return response()->json([
+                'status'    => $errCode,
+                'message'   => $errMessage
+            ], $errCode);
+        }
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,4 @@ namespace App\Http\Controllers\Api;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('/register', [AuthController::class, 'register']);
