f79f3db83967344eb1f0d3b87c6303b03fbdcf0b
darektoa <darektoa@gmail.com>
2021-12-12 21:55:09 +0700

    Added: Returns response json not found when route  model binding failed

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,6 +2,8 @@
 
 namespace App\Exceptions;
 
+use App\Helpers\ResponseHelper;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
 
@@ -37,5 +39,15 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+
+    public function render($request, Throwable $exception) {
+        if(
+            $exception instanceof ModelNotFoundException &&
+            $request->is('api/*')
+        ) return ResponseHelper::error(['Not found'], 'Not found', 404);
+
+        return parent::render($request, $exception);
     }
 }
