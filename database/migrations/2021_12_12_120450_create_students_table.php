de45815bca35a63363d770b2621f17fb8d05201e
darektoa <darektoa@gmail.com>
2021-12-12 19:12:28 +0700

    Added: Students table migration

---
+++ database/migrations/2021_12_12_120450_create_students_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->char('code', 8)->unique();
+            $table->string('name', 100);
+            $table->string('id_number', 20);
+            $table->date('birth_date');
+            $table->string('birth_place', 50);
+            $table->text('address');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
