270bb207844f9e80801e0708f2523fece657f050
darektoa <darektoa@gmail.com>
2021-12-12 11:19:10 +0700

    Edited: Nullabling name field in personal access token migration

--- routes/api.php
+++ routes/api.php
@@ -5,5 +5,6 @@ namespace App\Http\Controllers\Api;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('/auth')->group(function() {
+    Route::post('/login', [AuthController::class, 'login']);
     Route::post('/register', [AuthController::class, 'register']);
 });
