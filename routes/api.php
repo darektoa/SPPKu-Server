e1a5ab30710cf54d193c07e912419a573de64397
darektoa <darektoa@gmail.com>
2021-12-12 09:50:42 +0700

    Added: Auth prefix in authentication API route

--- routes/api.php
+++ routes/api.php
@@ -5,4 +5,6 @@ namespace App\Http\Controllers\Api;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/register', [AuthController::class, 'register']);
+Route::prefix('/auth')->group(function() {
+    Route::post('/register', [AuthController::class, 'register']);
+});
