a77745eb8b94366ef84441e91041b349e75a41e4
darektoa <darektoa@gmail.com>
2021-12-22 08:47:52 +0700

    Added: Store method for add a child in child API controller

--- app/Http/Controllers/Api/ChildController.php
+++ app/Http/Controllers/Api/ChildController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Exceptions\ErrorException;
 use App\Helpers\ResponseHelper;
 use App\Http\Controllers\Controller;
-use App\Models\User;
+use App\Models\{Student, User};
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class ChildController extends Controller
 {
@@ -18,5 +20,34 @@ class ChildController extends Controller
             ->get();
 
         return ResponseHelper::make($childs);
+    }
+
+
+    public function store(Request $request) {
+        try{
+            $validator  = Validator::make($request->all(), [
+                'code'  => 'required|size:8|exists:students,code'
+            ]);
+
+            if($validator->fails()) {
+                $errors = $validator->errors()->all();
+                throw new ErrorException('Unprocessable', $errors, 422);
+            }
+
+            $code     = $request->code;
+            $student  = Student::where('code', '=', $code)->first();
+            $payer    = auth()->user()->payer;
+            $child    = $payer->children()->firstOrCreate([
+                'student_id'  => $student->id,
+            ]);
+
+            return ResponseHelper::make($child);
+        }catch(ErrorException $err) {
+            return ResponseHelper::error(
+                $err->getErrors(),
+                $err->getMessage(),
+                $err->getCode(),
+            );
+        }
     }
 }
