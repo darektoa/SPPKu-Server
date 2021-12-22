2d49095f91a3abc50b08b19402bb9034f26522a0
darektoa <darektoa@gmail.com>
2021-12-22 08:25:14 +0700

    Added: Index method in child API controller

--- app/Http/Controllers/Api/ChildController.php
+++ app/Http/Controllers/Api/ChildController.php
@@ -2,10 +2,21 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Helpers\ResponseHelper;
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class ChildController extends Controller
 {
-    
+    public function index() {
+        $userId = auth()->user()->id;
+        $childs = User::with(['payer.children'])
+            ->find($userId)
+            ->payer
+            ->children()
+            ->get();
+
+        return ResponseHelper::make($childs);
+    }
 }
