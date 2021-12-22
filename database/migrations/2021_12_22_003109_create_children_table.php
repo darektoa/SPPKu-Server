779eb89be4a0a6730afca96360d098331438683e
darektoa <darektoa@gmail.com>
2021-12-22 08:06:35 +0700

    Edited: Rename childs table migration to children

---
+++ database/migrations/2021_12_22_003109_create_children_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateChildrenTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('children', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('payer_id')->constrained();
+            $table->foreignId('student_id')->constrained();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('children');
+    }
+}
